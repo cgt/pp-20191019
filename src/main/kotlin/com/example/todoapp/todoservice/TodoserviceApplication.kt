28059b9ea9f19bda3516eb57507ccf0dd4d66a08
Christoffer G. Thomsen <chris@cgt.name>
2019-10-19 19:04:19 +0200

    working

---
+++ src/main/kotlin/com/example/todoapp/todoservice/TodoserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.example.todoapp.todoservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<TodoserviceApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/todoapp/todoservice/TodoserviceApplicationTests.kt
@@ -0,0 +1,15 @@
+package com.example.todoapp.todoservice
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.core.JdbcTemplate
+import java.sql.ResultSet
+
+@SpringBootTest
+class TodoserviceApplicationTests {
+	@Test
+	fun contextLoads() {
+	}
+}
